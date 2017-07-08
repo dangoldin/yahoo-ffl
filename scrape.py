a0716068a60df4bb02344c7b60563976519c3563
Dan Goldin <dgoldin@triplelift.com>
2017-07-07 23:29:58 -0400

    Fix bad columns. Clean up team, position, and opponent. Still need to rerun on entire dataset

--- scrape.py
+++ scrape.py
@@ -8,36 +8,37 @@ import settings
 RE_REMOVE_HTML = re.compile('<.+?>')
 
 SLEEP_SECONDS = 3
-END_WEEK = 17
-PAGES_PER_WEEK = 4
+END_WEEK = 1
+PAGES_PER_WEEK = 1
 YAHOO_RESULTS_PER_PAGE = 25 # Static but used to calculate offsets for loading new pages
 
-fields = ['week','name','position', 'opp', 'passing_yds', 'passing_tds', 'passing_int',
-    'rushing_att', 'rushing_yds', 'rushing_tds', 'receiving_tgt', 'receiving_rec', 'receiving_yds', 'receiving_tds',
-    'return_tds', 'twopt', 'fumbles', 'points' ]
+fields = ['week', 'name', 'position', 'team', 'opp', 'passing_yds', 'passing_tds',
+    'passing_int', 'rushing_att', 'rushing_yds', 'rushing_tds', 'receiving_tgt',
+    'receiving_rec', 'receiving_yds', 'receiving_tds', 'return_tds', 'twopt',
+    'fumbles', 'points' ]
 
 XPATH_MAP = {
     'name': 'td[contains(@class,"player")]/div/div/div[contains(@class,"ysf-player-name")]/a',
     'position': 'td[contains(@class,"player")]/div/div/div[contains(@class,"ysf-player-name")]/span',
     'opp': 'td//div[contains(@class,"ysf-player-detail")]/span/a',
 
-    'passing_yds': 'td[10]',
-    'passing_tds': 'td[11]',
-    'passing_int': 'td[12]',
+    'passing_yds': 'td[11]',
+    'passing_tds': 'td[12]',
+    'passing_int': 'td[13]',
 
-    'rushing_att': 'td[13]',
-    'rushing_yds': 'td[14]',
-    'rushing_tds': 'td[15]',
+    'rushing_att': 'td[14]',
+    'rushing_yds': 'td[15]',
+    'rushing_tds': 'td[16]',
 
-    'receiving_tgt': 'td[16]',
-    'receiving_rec': 'td[17]',
-    'receiving_yds': 'td[18]',
-    'receiving_tds': 'td[19]',
+    'receiving_tgt': 'td[17]',
+    'receiving_rec': 'td[18]',
+    'receiving_yds': 'td[19]',
+    'receiving_tds': 'td[20]',
 
-    'return_tds': 'td[20]',
-    'twopt': 'td[21]',
-    'fumbles': 'td[22]',
-    'points': 'td[6]',
+    'return_tds': 'td[21]',
+    'twopt': 'td[22]',
+    'fumbles': 'td[23]',
+    'points': 'td[7]',
 }
 
 stats = []
@@ -47,6 +48,11 @@ def process_stats_row(stat_row, week):
     stats_item['week'] = week
     for col_name, xpath in XPATH_MAP.items():
         stats_item[col_name] = RE_REMOVE_HTML.sub('', stat_row.find_element_by_xpath(xpath).get_attribute('innerHTML'))
+    # Custom logic for team, position, and opponent
+    stats_item['opp'] = stats_item['opp'].split(' ')[-1]
+    team, position = stats_item['position'].split(' - ')
+    stats_item['position'] = position
+    stats_item['team'] = team
     return stats_item
 
 def process_page(driver, week, cnt):
